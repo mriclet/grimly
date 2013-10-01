b2c96d9845b422dede469adbff6fc8b4bbe0e540
ienien <jblanche@e3r10p10.42.fr>
2013-10-01 20:06:44 +0200

    correction nb cols

--- srcs/map_info.c
+++ srcs/map_info.c
@@ -6,25 +6,38 @@
 /*   By: jblanche <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/09/30 16:15:11 by jblanche          #+#    #+#             */
-/*   Updated: 2013/10/01 18:07:41 by jblanche         ###   ########.fr       */
+/*   Updated: 2013/10/01 20:04:40 by jblanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "grimly.h"
-void	store_param(t_info *info, int i);
+int		store_param(t_info *info, int i);
 
 void	map_info(t_info *info)
 {
 	int		i;
+	int		j;
+	char	*nb_cols;
 	char	*nb_lines;
 	int		length_line;
+	int		length_cols;
 
 	i = 0;
+	j = 0;
 	while (info->param_line[i] != '\n')
 		i++;
-	info->nb_cols = i;
 	i--;
-	store_param(info, i);
+	i = store_param(info, i);
+	while (info->param_line[i--] != 'x')
+		length_cols++;
+	nb_cols = (char*)malloc(sizeof (*nb_cols) * length_cols);
+	while (i < length_cols)
+	{
+		nb_cols[j] = info->param_line[i];
+		j++;
+		i++;
+	}
+	free(nb_cols);
 	length_line = 0;
 	while (info->param_line[length_line] != 'x')
 		length_line++;
@@ -35,11 +48,12 @@ void	map_info(t_info *info)
 		nb_lines[i] = info->param_line[i];
 		i++;
 	}
+	info->nb_cols = ft_char_to_int(nb_cols);
 	info->nb_lines = ft_char_to_int(nb_lines);
 	free(nb_lines);
 }
 
-void	store_param(t_info *info, int i)
+int		store_param(t_info *info, int i)
 {
 	info->param = malloc(sizeof (char) * 4);
 	info->param[4] = info->param_line[i--];
@@ -47,4 +61,5 @@ void	store_param(t_info *info, int i)
 	info->param[2] = info->param_line[i--];
 	info->param[1] = info->param_line[i--];
 	info->param[0] = info->param_line[i--];
+	return (i);
 }
